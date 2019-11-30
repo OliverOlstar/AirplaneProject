1783209c28c670a1adb35865650c351ae119d27c
Oliver <Olstar_123@users.noreply.github.com>
2019-11-29 21:14:07 -0500

    Added Lerping Visuals

---
+++ HeliPhysics/Assets/Scripts/PlaneVisuals.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneVisuals : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float rotationDampening = 5;
+    [SerializeField] private float positionDampening = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = _target.position;
+        transform.rotation = _target.rotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, _target.rotation, Time.deltaTime * rotationDampening);
+        transform.position = Vector3.Lerp(transform.position, _target.position, Time.deltaTime * positionDampening);
+    }
+}
