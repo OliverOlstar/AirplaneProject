c8a8c5e83f5d12bcc4db9e56eb7bbe476347cf16
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2019-12-07 14:45:14 -0500

    Redid Wheels

--- HeliPhysics/Assets/Scripts/PlaneWheels.cs
+++ HeliPhysics/Assets/Scripts/PlaneWheels.cs
@@ -6,13 +6,15 @@ public class PlaneWheels : MonoBehaviour
 {
     private PlanePhysics physics;
     [SerializeField] private Vector3 raycastDirection;
+    private Vector3 relativeRaycastDirection;
     [SerializeField] private float raycastLength;
     [SerializeField] private Vector3 raycastStartOffset;
-    [SerializeField] [Range(0, 0.5f)] private float raycastExtraLength;
+    [SerializeField] [Range(0, 1f)] private float raycastExtraLength;
     [SerializeField] private float raycastExtraMult;
     [SerializeField] private float raycastMult;
 
     [SerializeField] private float coefficientOfFriction = 0.4f;
+    [SerializeField] private float maxFriction = 0.4f;
     [SerializeField] [Range(0, 1)] private float bounciness;
 
     [Space]
@@ -28,14 +30,16 @@ public class PlaneWheels : MonoBehaviour
 
     public void CheckWithWheel()
     {
+        relativeRaycastDirection = transform.TransformDirection(raycastDirection).normalized;
+
         RaycastHit hit;
-        if (Physics.Raycast(transform.position + raycastStartOffset, raycastDirection.normalized, out hit, raycastLength))
+        if (Physics.Raycast(transform.position + raycastStartOffset, relativeRaycastDirection, out hit, raycastLength))
         {
             //Normal Force
             Vector3 normalForce = CalculateNormal(hit);
 
             //Friction
-            //normalForce += CalculateFriction();
+            normalForce += CalculateFriction() * Time.deltaTime;
 
             physics._angluarVelocity += rotation * normalForce.magnitude * Time.deltaTime * rotationMult;
             if (Debugs) Debug.Log(normalForce.magnitude * Time.deltaTime * rotationMult);
@@ -61,19 +65,20 @@ public class PlaneWheels : MonoBehaviour
 
     private Vector3 CalculateFriction()
     {
-        Vector3 friction = coefficientOfFriction * physics._velocity;
-        if (friction.magnitude <= 0.002f) 
-            return physics._velocity;
+        Vector3 friction = Vector3.Min(coefficientOfFriction * physics._velocity, maxFriction * physics._velocity.normalized);
+        if (friction.magnitude <= 0.005f) 
+            return -physics._velocity;
         else
-            return friction;
+            return -friction;
 
         // TODO Add Cut off at small enough number
     }
 
     private void OnDrawGizmos()
     {
+        relativeRaycastDirection = transform.TransformDirection(raycastDirection).normalized;
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position + raycastStartOffset, transform.position + raycastStartOffset + (raycastDirection.normalized * raycastLength));
-        Gizmos.DrawSphere(transform.position + raycastStartOffset + (raycastDirection.normalized * raycastExtraLength), 0.03f);
+        Gizmos.DrawLine(transform.position + raycastStartOffset, transform.position + raycastStartOffset + (relativeRaycastDirection * raycastLength));
+        Gizmos.DrawSphere(transform.position + raycastStartOffset + (relativeRaycastDirection * raycastExtraLength), 0.03f);
     }
 }
